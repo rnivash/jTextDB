8edfc99a41d70c5cf4ea63b89eed7a8faece262e
Nivash Ramachandran <nivash_ramachandran@dell.com>
2020-06-07 23:49:14 +0530

    Add unit test case for base file

--- textDB.test/src/textDB/test/BaseFileTests.java
+++ textDB.test/src/textDB/test/BaseFileTests.java
@@ -10,13 +10,40 @@ import textDB.Model.Student;
 class BaseFileTests {
 
 	@Test
-	void test() {
-		Student st = new Student();
-		String v1 = BaseFile.GetFullName(st.getClass());
-		System.out.print(v1);
+	void Should_generate_file_path() {		
 		
-		assertEquals("Name", v1);
+		String filePath = BaseFile.GetFullName(Student.class);
 		
+		assertEquals(true, filePath.contains("Student.-1975158926.db1"));
+		
+	}
+	
+	@Test
+	void Should_generate_hash_code_from_type() {		
+		
+		int fileId = BaseFile.GetId(Student.class);
+		
+		assertEquals(-1975158926, fileId);		
+	}
+	
+	@Test
+	void Should_encode_file_content() {		
+		
+		String[] rowData = new String[] {"Test", "24", "45", "Apple,Orange"};
+		String encodeString = BaseFile.Encode(rowData);
+		
+		assertEquals("Test,24,45,Apple#comma#Orange", encodeString);		
+	}
+	
+	@Test
+	void Should_decode_file_content() {		
+		
+		String[] rowData = BaseFile.Decode("Test,24,45,Apple#comma#Orange");
+		
+		assertEquals("Test", rowData[0]);
+		assertEquals("24", rowData[1]);
+		assertEquals("45", rowData[2]);
+		assertEquals("Apple,Orange", rowData[3]);
 	}
 
 }
